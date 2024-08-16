6ce5f91624cf9f2b1725efeb4038ad2df500f5fa
rinjugatla <rin.jugatla@gmail.com>
2024-08-16 16:57:22 +0900

    emailでのログインを独自実装
    auth-ui-svelteではログイン、ログアウトを検知できない？

---
+++ chat/src/lib/components/supabase/Auth.svelte
@@ -0,0 +1,166 @@
+<script lang="ts">
+	import { Button, Input, Label } from 'flowbite-svelte';
+	import { session, supabase } from '$lib/store';
+	import type { Session } from '@supabase/supabase-js';
+	import { createEventDispatcher, onMount } from 'svelte';
+	import { get } from 'svelte/store';
+
+	let email = '';
+	let password = '';
+	let localSession: Session | null = null;
+	const dispatch = createEventDispatcher();
+	/**
+	 * 現在サインイン中か
+	 */
+	export let isSignIned = !!localSession;
+	/**
+	 * サインインモード
+	 */
+	let signMode: 'in' | 'up' = 'in';
+
+	onMount(async () => {
+		const { data, error } = await get(supabase).auth.getSession();
+		if (error) {
+			return;
+		}
+
+		storeSession(data.session);
+	});
+
+	const storeSession = (_session: Session | null) => {
+		localSession = _session;
+		session.set(_session);
+
+		if (isSignIned) {
+			dispatch('signin');
+		} else {
+			dispatch('signout');
+		}
+	};
+
+	/**
+	 * サインイン
+	 */
+	const signInWithEmail = async () => {
+		const { data, error } = await get(supabase).auth.signInWithPassword({
+			email: email,
+			password: password
+		});
+
+		if (error) {
+			alert(error.message);
+			return;
+		}
+
+		storeSession(data.session);
+	};
+
+	/**
+	 * サインアップ
+	 */
+	const signUpWithEmail = async () => {
+		const { data, error } = await get(supabase).auth.signUp({
+			email: email,
+			password: password
+		});
+
+		if (error) {
+			alert(error.message);
+			return;
+		}
+
+		const isVerified = data.session;
+		if (!isVerified) {
+			alert('Please check your inbox for email verification!');
+			return;
+		}
+
+		storeSession(data.session);
+	};
+
+	/**
+	 * サインアウト
+	 */
+	const signOut = async () => {
+		const { error } = await get(supabase).auth.signOut();
+		if (error) {
+			alert(error.message);
+			return;
+		}
+
+		storeSession(null);
+	};
+</script>
+
+<div class="m-5">
+	{#if isSignIned}
+		<Button
+			on:click={async () => {
+				await signOut();
+			}}>Signout</Button
+		>
+	{:else}
+		<div class="flex"></div>
+		{#if signMode === 'in'}
+			<form
+				on:submit|preventDefault={async () => {
+					signInWithEmail();
+				}}
+			>
+				<div class="flex">
+					<Label for="email" class="mx-2 content-center">Email address</Label>
+					<Input type="text" id="email" placeholder="Email address" bind:value={email} required />
+				</div>
+				<div class="flex">
+					<Label for="password" class="mx-2 content-center">Your Password</Label>
+					<Input
+						type="password"
+						id="password"
+						placeholder="Your Password"
+						bind:value={password}
+						required
+					/>
+				</div>
+				<div class="flex gap-2">
+					<Button type="submit">Sign In</Button>
+					<Button
+						color="green"
+						on:click={() => {
+							signMode = 'up';
+						}}>Sign Up</Button
+					>
+				</div>
+			</form>
+		{:else}
+			<form
+				on:submit|preventDefault={async () => {
+					await signUpWithEmail();
+				}}
+			>
+				<div class="flex">
+					<Label for="email" class="mx-2 content-center">Email address</Label>
+					<Input type="text" id="email" placeholder="Email address" bind:value={email} required />
+				</div>
+				<div class="flex">
+					<Label for="password" class="mx-2 content-center">Create Password</Label>
+					<Input
+						type="password"
+						id="password"
+						placeholder="Create Password"
+						bind:value={password}
+						required
+					/>
+				</div>
+				<div class="flex gap-2">
+					<Button type="submit">Sign Up</Button>
+					<Button
+						color="green"
+						on:click={() => {
+							signMode = 'in';
+						}}>Sign In</Button
+					>
+				</div>
+			</form>
+		{/if}
+	{/if}
+</div>
--- chat/src/routes/+page.svelte
+++ chat/src/routes/+page.svelte
@@ -1,23 +1,5 @@
 <script lang="ts">
-	import { session, supabase } from '$lib/store';
-	import { onMount } from 'svelte';
-    import { Auth } from '@supabase/auth-ui-svelte';
-	import { get } from 'svelte/store';
-	import { ThemeSupa } from '@supabase/auth-ui-shared';
-
-	onMount(async () => {
-		const { data, error } = await get(supabase).auth.getSession();
-		if (error) {
-			console.log(error);
-			return;
-		}
-
-		session.set(data.session);
-	});
+	import Auth from '$lib/components/supabase/Auth.svelte';
 </script>
 
-{#if get(session)}
-	<div>Logged in!</div>
-{:else}
-	<Auth supabaseClient={get(supabase)} appearance={{ theme: ThemeSupa }}></Auth>
-{/if}
+<Auth />
